857ea5e4554fa7e926159ab91f29b98102b83320
junho0957 <juo0957@naver.com>
2024-06-07 12:24:08 +0900

    20232057 송준호

---
+++ chp13/grade.h
@@ -0,0 +1,17 @@
+#pragma once
+// grade.h
+
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+#include <list>
+#include <vector> 
+using namespace std;
+
+
+double grade(double, double, double);
+double grade(double, double, vector<double>&); 
+
+
+
+#endif
